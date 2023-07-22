41cb994acc7427d80015369f9fc2f3f89784c849
ThomasCharuel <ThomasCharuel@users.noreply.github.com>
2023-07-22 18:53:48 +0200

    Highlight active route in sidebar

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -4,8 +4,19 @@ import { Button } from '@/components/ui/button';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { Menu } from 'lucide-react';
 import Sidebar from '@/components/sidebar';
+import { useEffect, useState } from 'react';
 
 const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  // Avoid hydration error
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
   return (
     <Sheet>
       <SheetTrigger>
